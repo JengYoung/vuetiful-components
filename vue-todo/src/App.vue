0ae7d037427bb06ffbee13b421f24ba827937de3
JengYoung <young_pallete@naver.com>
2022-01-09 11:53:24 +0900

    chore: set nvmrc

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,29 +1,13 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <h1>TodoList + TypeScript</h1>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
 
-export default Vue.extend({
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
-});
+export default Vue.extend({});
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style scoped></style>
--- vue-todo/src/main.ts
+++ vue-todo/src/main.ts
@@ -1,5 +1,12 @@
 import Vue from 'vue';
 import App from './App.vue';
+/*
+ * TODO: 투두 리스트 만들기
+ - 1. 할 일 추가
+ - 2. 할 일 조회
+ - 3. 할 일 삭제
+ - 4. 할 일 수정
+ */
 
 Vue.config.productionTip = false;
 
