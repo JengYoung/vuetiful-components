643940e37471c436910dc1d6f1872d38f5ef0bef
JengYoung <young_pallete@naver.com>
2022-01-09 12:39:55 +0900

    feat: v-model 대신 emit을 통한 인풋 데이터 업데이트 기능 구현

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,13 +1,28 @@
 <template>
   <div>
     <h1>TodoList + TypeScript</h1>
+    <TodoInput :todoText="todoText" @updateTodoText="updateTodoText"></TodoInput>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import TodoInput from './components/TodoInput.vue';
 
-export default Vue.extend({});
+export default Vue.extend({
+  components: { TodoInput },
+  data() {
+    return {
+      todoText: '',
+    };
+  },
+  methods: {
+    updateTodoText(value: string) {
+      console.log('value: ', value);
+      this.todoText = value;
+    },
+  },
+});
 </script>
 
 <style scoped></style>
---
+++ vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <label for="todo-input"></label>
+    <input id="todo-input" type="text" :value="todoText" @input="handleInput" />
+    <button @click="addTodo">add</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  props: ['todoText'],
+  methods: {
+    handleInput(e: Event) {
+      this.$emit('updateTodoText', (e.target as HTMLInputElement).value);
+    },
+    addTodo() {
+      console.log('add');
+    },
+  },
+});
+</script>
+
+<style scoped></style>
